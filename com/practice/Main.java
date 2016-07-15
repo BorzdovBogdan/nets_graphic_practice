de8be749293af12d00a4b55514ea714f9437d23b
Kolya Nesterov <shadow_blast@mail.ru>
2016-07-15 17:08:42 +0300

    bolshaya igra

--- com/practice/Main.java
+++ com/practice/Main.java
@@ -3,7 +3,6 @@ package nets_graphic_practice.com.practice;
 import nets_graphic_practice.com.practice.model.GameMap;
 import nets_graphic_practice.com.practice.model.GameMapReader;
 import nets_graphic_practice.com.practice.view.GameWindow;
-<<<<<<< HEAD
 public class Main {
         public static void main(String[] args) {
                 GameMapReader gmr = new GameMapReader(32,3);
@@ -11,14 +10,3 @@ public class Main {
                 GameWindow gw = new GameWindow(gameMap);
         }
 }
-=======
-
-import java.util.ArrayList;
-public class Main {
-    public static void main(String[] args) {
-        GameMapReader gmr = new GameMapReader(32,3);
-        GameMap gameMap = new GameMap(gmr);
-        GameWindow gw = new GameWindow(gameMap);
-    }
-}
->>>>>>> d5dd83c0bf0b1d4b46738808090c70a908272f0e
